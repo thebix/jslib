536deb8f40911429ff3b6efcafc80b2ee01d4284
Pavel Biriukov <pbiryukov@beeline.ru>
2017-09-03 13:02:50 +0300

    removed server subscription

--- serverDebug.js
+++ serverDebug.js
@@ -4,7 +4,3 @@ import lib from './root'
 console.log(`Start server ${_config.isProduction ? '<Production>' : '<Debug>'}`)
 
 console.log(lib.time.dateTimeString())
-
-lib.www.response.subscribe(x =>
-    console.log(x)
-)
