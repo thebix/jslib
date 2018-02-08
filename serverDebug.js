6e31f53d5f855b88089bced9e5e95dca75b8252d
Pavel Biriukov <syntheticforms@gmail.com>
2018-02-09 00:24:53 +0300

    changed filename http-server to httpServer, removed unused config relation

--- serverDebug.js
+++ serverDebug.js
@@ -1,6 +1,7 @@
-import _config from './config'
 import lib from './root'
 
-console.log(`Start server ${_config.isProduction ? '<Production>' : '<Debug>'}`)
+const isProduction = process.env.NODE_ENV === 'production'
+
+console.log(`Start server ${isProduction ? '<Production>' : '<Debug>'}`)
 
 console.log(lib.time.dateTimeString())
